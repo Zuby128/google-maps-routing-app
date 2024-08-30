91150ea7f2646a3e466f7870cefb2b2b2bfeca8a
zberber20 <zberber20@gmail.com>
2024-08-30 17:25:56 +0300

    new folder design added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Map from "./components/Map";
+import Map from "../components/Map";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Map />
-    </div>
+    </>
   );
 }
